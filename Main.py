810d1b9d055ee0f39e305505f184b08a023d4e2c
matthew42904 <matthew42904@gmail.com>
2023-08-09 17:57:02 -0400

    sssssss

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from sqlite3 import Error
 
 
 try:
-    conn = sqlite3.connect('Tripi.db')
+    conn = sqlite3.connect('./Tripi.db')
 except Error as e:
     print(e)
 
@@ -12,7 +12,7 @@ while True:
     prod_1_manufature = str(input("Manufature Name: "))
     prod_1_item = str(input("Item Name: "))
     prod_1_TripiNumber= str(input("Tripi Number: "))
-    # add to tripi with a input: 
+    #add to tripi with a input: 
     c.execute("INSERT INTO tripi VALUES (?, ?, ?)", (prod_1_manufature, prod_1_item, prod_1_TripiNumber))
 
     conn.commit()
@@ -21,6 +21,7 @@ while True:
 
 # make table: 
 #c.execute("""CREATE TABLE tripi (manufacture text, item text,TripiCode text)""")
+#conn.commit()
 
 # add to tripi with a pepsi manufature test item and a item number of 15699: 
 #c.execute("""INSERT INTO tripi VALUES ("pepsi",'test5', "15669")""")
