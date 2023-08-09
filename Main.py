396037a9fa9dc395100373f3d1a45098d81d1518
matthew42904 <matthew42904@gmail.com>
2023-08-09 17:49:47 -0400

    second

--- Main.py
+++ Main.py
@@ -7,12 +7,17 @@ try:
 except Error as e:
     print(e)
 
+c = conn.cursor()
 
 prod_1_manufature = str(input("Manufature Name: "))
 prod_1_item = str(input("Item Name: "))
 prod_1_TripiNumber= str(input("Tripi Number: "))
+# add to tripi with a input: 
+c.execute("INSERT INTO tripi VALUES (?, ?, ?)", (prod_1_manufature, prod_1_item, prod_1_TripiNumber))
 
-c = conn.cursor()
+conn.commit()
+
+
 
 # make table: 
 #c.execute("""CREATE TABLE tripi (manufacture text, item text,TripiCode text)""")
@@ -20,10 +25,7 @@ c = conn.cursor()
 # add to tripi with a pepsi manufature test item and a item number of 15699: 
 #c.execute("""INSERT INTO tripi VALUES ("pepsi",'test5', "15669")""")
 
-# add to tripi with a input: 
-c.execute("INSERT INTO tripi VALUES (?, ?, ?)", (prod_1_manufature, prod_1_item, prod_1_TripiNumber))
 
-conn.commit()
 
 #c.execute("""SELECT * FROM tripi WHERE manufacture='pepsi'""")
 
